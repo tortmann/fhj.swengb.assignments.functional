f4b89e2a58909528b34365aa3c9aab1fb0d88247
tortmann <thomas.ortmann@edu.fh-joanneum.at>
2015-10-29 12:39:53 +0100

    solution v1.0

--- src/main/scala/fhj/swengb/assignments/functional/tortmann/FunctionalAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/functional/tortmann/FunctionalAssignment.scala
@@ -1,5 +1,6 @@
 package fhj.swengb.assignments.functional.tortmann
 
+
 /**
   * This is the first assignment for swengb 2015.
   *
@@ -19,12 +20,12 @@ case class FunctionalAssignment(lowerBound: Int = 1,
     * xs(i) + factor == xs(i + 1) (for i > 0 and i <= 1000)
     *
     */
-  val xs: Seq[Int] = ???
+  val xs: Seq[Int] = lowerBound until upperBound+1
 
   /**
     * provide the sum of all elements defined in xs
     */
-  val sumXs = ???
+  val sumXs = xs.sum
 
   /**
     * This sequence contains a list with the following elements:
@@ -39,23 +40,22 @@ case class FunctionalAssignment(lowerBound: Int = 1,
     * x(999) = lowerbound
     *
     */
-  val ys: Seq[Int] = ???
+  val ys: Seq[Int] = xs.reverse.map(_*factor)
 
   /**
     * this sequence contains pairs of xs and ys. the ys should
     * be in reversed order.
     */
-  val zs: Seq[(Int, Int)] = ???
+  val zs: Seq[(Int, Int)] = xs zip ys.reverse
 
   /**
     * Function should divide the second operand with the first.
     */
-  def f(ab: (Int, Int)): Int = ???
+  def f(ab: (Int, Int)): Int = ab._2 / ab._1
 
   /**
     * apply function f defined above to all elements of zs, return the sequence
     */
-  val result: Seq[Int] = ???
+  val result: Seq[Int] = zs.map(f)
 
 }
-
